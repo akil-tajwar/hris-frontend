6bcf3e7738608757e116dad9efd36790a1328b97
Akil Tajwar <akiltajwar@gmail.com>
2026-05-06 11:05:00 +0600

    refactor: nextjs version update

--- hris-backend/src/routes/weekends.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { getWeekendsController } from '../controllers/weekdends.controller'
-import { authenticateUser } from '../middlewares/auth.middleware'
-
-const router = Router()
-
-router.get('/getAll', authenticateUser, getWeekendsController)
-
-export default router
