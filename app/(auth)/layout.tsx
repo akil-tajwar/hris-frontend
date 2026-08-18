fd4093a65619a778f025ed4420ff911d0134b3f9
Akil Tajwar <akiltajwar@gmail.com>
2026-08-18 16:01:24 +0600

    feat: solving build issues

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from '@/components/ui/toaster'
-// import '.././globals.css'
-import '../globals.css'
+import '.././globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
